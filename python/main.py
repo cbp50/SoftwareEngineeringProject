f5456a2eb232813bff05faa5e5ea27aca18a5618
crm222 <78119067+crm222@users.noreply.github.com>
2021-04-13 16:50:04 -0500

    Add files via upload

---
+++ python/main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+df = pd.read_csv('data.csv')
+print(df.describe())
+    
+
+
+
+
+
